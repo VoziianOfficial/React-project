046e732de89ffd37c1f6e059b7594567da623011
Yuliia Voziian <vozianulia@gmail.com>
2024-10-14 23:58:45 +0300

    add some components

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import s from "./MyPosts.module.css";
+import Post from "../Post/Post";
 
 const MyPosts = () => {
   return (
     <>
       <div>
-        <textarea></textarea>
-        <button>Add Post</button>
-        <button>Remove</button>
-
-        <div>New post</div>
-        <div className={s.postsItem}>
-          <div className={s.item}>
-            <img src="/src/img/dog.jpg" alt="dog" />
-            post one
-          </div>
-          <div>post two</div>
-          <div>post three</div>
+        My Posts
+        <div>
+          <textarea></textarea>
+          <button>Add Post</button>
+          <button>Remove</button>
+        </div>
+        <div className="s.posts">
+          <Post message="Hi, how are you?" likeCount={5} />
+          <Post message="It's my first post" likeCount={11} />
         </div>
       </div>
     </>
---
+++ src/components/Profile/Post/Post.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import s from "./Post.module.css";
+
+const Post = (props) => {
+  return (
+    <>
+      <div className={s.item}>
+        <img src="/src/img/dog.jpg" alt="dog" />
+        {props.message}
+        <div>
+          <span>Like:</span>
+          {props.likeCount}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Post;
